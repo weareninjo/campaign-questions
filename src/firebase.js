74008aca6f02bf8819f557c2d00ddef92e911a04
Patrick Twambilile Sibale <twambililesibale@gmail.com>
2022-03-04 18:03:07 +0300

    Fixed an error caused by the plugin and replaced firebaseJS with ninjo firebase

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,15 @@ import "firebase/compat/storage";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBMgSs2SkEl0L1IJ8d1VWtq62mJwRazGjg",
-    authDomain: "campaign-questions.firebaseapp.com",
-    projectId: "campaign-questions",
-    storageBucket: "campaign-questions.appspot.com",
-    messagingSenderId: "980229935860",
-    appId: "1:980229935860:web:5d192caac80649a5e1c4a1",
-    measurementId: "G-XRQ43Y3MNJ"
-  };
+  apiKey: "AIzaSyBMgSs2SkEl0L1IJ8d1VWtq62mJwRazGjg",
+  authDomain: "ninjo-99ddf.firebaseapp.com",
+  databaseURL: "https://ninjo-99ddf-default-rtdb.firebaseio.com",
+  projectId: "ninjo-99ddf",
+  storageBucket: "ninjo-99ddf.appspot.com",
+  messagingSenderId: "980229935860",
+  appId: "1:980229935860:web:5d192caac80649a5e1c4a1",
+  measurementId: "G-XRQ43Y3MNJ"
+};
   
   // Initialize Firebase
 const app = initializeApp(firebaseConfig);
